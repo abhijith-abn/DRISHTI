51656ddedd0eca31c9bec2be57d1f5ffe07115cb
Abhijith-abn <abhijithcoding@gmail.com>
2026-04-07 17:34:56 +0530

    Initial commit: Added Auth and Course Selection with Booking Modal

---
+++ drishti-backend/drishti/src/main/java/com/example/drishti/DrishtiApplication.java
@@ -0,0 +1,13 @@
+package com.example.drishti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrishtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrishtiApplication.class, args);
+	}
+
+}
---
+++ drishti-backend/drishti/src/test/java/com/example/drishti/DrishtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drishti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrishtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
